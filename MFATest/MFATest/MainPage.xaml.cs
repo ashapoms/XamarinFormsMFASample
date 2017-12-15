bff990f5fb2daaaa6da00129a58be052eaf73628
Alexander Shapoval <ashapoms@hotmail.com>
2017-12-15 11:07:51 +0300

    'Tasks lists' and 'Add task' buttons and logic added

--- MFATest/MFATest/MainPage.xaml.cs
+++ MFATest/MFATest/MainPage.xaml.cs
@@ -15,7 +15,7 @@ namespace MFATest
     {
         public static string clientId = "4c2d7c93-9268-49ea-a31c-da3ca5032922";
         public static string authority = "https://login.windows.net/common";
-        public static string returnUri = "http://MFATestPCL-redirectTest";
+        public static string returnUri = "http://MFATestPCL-redirect";
         private const string graphResourceUri = "https://graph.windows.net";
         private AuthenticationResult authResult = null;
 
@@ -76,9 +76,10 @@ namespace MFATest
             if (!(authResult is null))
             {
                 client.MaxResponseContentBufferSize = 256000;
-                var uri = new Uri(string.Format("http://centos-srv01.westeurope.cloudapp.azure.com:3000/tasks", string.Empty));
-                string authHeader = authResult.CreateAuthorizationHeader();
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authHeader);
+                var uri = new Uri(string.Format("http://centos-srv01.westeurope.cloudapp.azure.com:3000/tasks/sevstaluser01", string.Empty));
+                // string authHeader = authResult.CreateAuthorizationHeader();
+                // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authHeader);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
 
                 try
                 {
@@ -99,5 +100,39 @@ namespace MFATest
             else
                 lblMessage.Text = "Please press Login to Azure button first";
         }
+
+        private async void btnAddTaskRestAdal_Clicked(object sender, EventArgs e)
+        {
+
+            if (!(authResult is null))
+            {
+                client.MaxResponseContentBufferSize = 256000;
+                var uri = new Uri(string.Format("http://centos-srv01.westeurope.cloudapp.azure.com:3000/tasks/sevstaluser01/task01", string.Empty));
+                // string authHeader = authResult.CreateAuthorizationHeader();
+                // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authHeader);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+                try
+                {
+                    string mydata = "test01";
+                    StringContent queryString = new StringContent(mydata);
+                    var response = await client.PostAsync(uri, queryString);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //lblMessage.Text = "REST Call Success";
+                        lblMessage.Text = await response.Content.ReadAsStringAsync();
+                        //var content = await response.Content.ReadAsStringAsync();
+                        //Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"ERROR: {ex.Message}";
+                }
+            }
+            else
+                lblMessage.Text = "Please press Login to Azure button first";
+        }
+
     }
 }
