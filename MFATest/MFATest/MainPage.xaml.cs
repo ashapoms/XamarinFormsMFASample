5d54e2d935686a572b3ed6c8b06f07151855bf73
Alexander Shapoval <ashapoms@hotmail.com>
2017-12-21 11:34:42 +0300

    User Login added

--- MFATest/MFATest/MainPage.xaml.cs
+++ MFATest/MFATest/MainPage.xaml.cs
@@ -18,6 +18,8 @@ namespace MFATest
         public static string returnUri = "http://MFATestPCL-redirect";
         private const string graphResourceUri = "https://graph.windows.net";
         private AuthenticationResult authResult = null;
+        private static string userLogin = null;
+        private static string RestUri = "http://centos-srv01.westeurope.cloudapp.azure.com:3000/tasks/";
 
         HttpClient client = new HttpClient();
 
@@ -33,10 +35,13 @@ namespace MFATest
             {
                 var auth = DependencyService.Get<IAuthenticator>();
                 authResult = await auth.Authenticate(authority, graphResourceUri, clientId, returnUri);
+                string userUpn = authResult.UserInfo.DisplayableId.ToString();
+                int userAt = userUpn.IndexOf('@');
+                userLogin = userUpn.Substring(0, userAt);
                 var userName = authResult.UserInfo.GivenName + " " + authResult.UserInfo.FamilyName;
                 lblUserName.Text = userName;
                 lblMessage.Text = "Access Token: " + authResult.AccessToken.ToString();
-                await DisplayAlert("Token", userName, "Ok", "Cancel");
+                await DisplayAlert("User Login", userLogin, "Ok", "Cancel");
             }
             catch (Exception ex)
             {
@@ -53,6 +58,38 @@ namespace MFATest
                 var auth = DependencyService.Get<IAuthenticator>();
                 auth.ClearAllCookies();
             }
+        }
+
+        private async void btnCallRestAdal_Clicked(object sender, EventArgs e)
+        {
+
+            if (!(authResult is null))
+            {
+                client.MaxResponseContentBufferSize = 256000;
+                // var uri = new Uri(string.Format("http://centos-srv01.westeurope.cloudapp.azure.com:3000/tasks/sevstaluser01", string.Empty));
+                var uri = new Uri(string.Format(RestUri + userLogin, string.Empty));
+                // string authHeader = authResult.CreateAuthorizationHeader();
+                // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authHeader);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+                try
+                {
+                    var response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //lblMessage.Text = "REST Call Success";
+                        lblMessage.Text = await response.Content.ReadAsStringAsync();
+                        //var content = await response.Content.ReadAsStringAsync();
+                        //Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"ERROR: {ex.Message}";
+                }
+            }
+            else
+                lblMessage.Text = "Please press Login to Azure button first";
         }
 
         private async void btnAddTaskRestAdal_Clicked(object sender, EventArgs e)
